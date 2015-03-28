068442160b5f21e1697c46e49300913c3e33bf8a
unknown <Administrator@ix.jenkinslan.com>
2015-03-28 09:52:04 +0000

    Initial commit

---
+++ Controller/PicLearningAndExamples/PICkit_3_Debug_Express_Lessons_X/01_Hello_LED.X/build/default/production/01_Hello_LED.p1.d
@@ -0,0 +1,4 @@
+ build/default/production/01_Hello_LED.d  \
+ build/default/production/01_Hello_LED.p1:  \
+ 01_Hello_LED.c  \
+../compilertype.h 
---
+++ Controller/PicLearningAndExamples/PICkit_3_Debug_Express_Lessons_X/07_ADC.X/build/default/production/07_ADC.p1.d
@@ -0,0 +1,5 @@
+ build/default/production/07_ADC.d  \
+ build/default/production/07_ADC.p1:  \
+ 07_ADC.c  \
+07_ADC.h  \
+../compilertype.h 
